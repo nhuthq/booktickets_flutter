82320be4e99b5f303269f791a5181541081bbd25
nhuthq <>
2023-04-10 12:19:19 +0700

    add defaul bottom bar

---
+++ booktickets/lib/screens/bottom_bar.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class BottomBar extends StatefulWidget {
+  const BottomBar({super.key});
+
+  @override
+  State<BottomBar> createState() => _BottomBarState();
+}
+
+class _BottomBarState extends State<BottomBar> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Book Tickets"),
+      ),
+      body: const Center(
+        child: Text("My Body"),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+          BottomNavigationBarItem(icon: Icon(Icons.search), label: "Search"),
+          BottomNavigationBarItem(icon: Icon(Icons.airplane_ticket), label: "Tickets"),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: "Profile")
+        ],
+      ),
+    );
+  }
+}
