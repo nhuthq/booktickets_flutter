0c39652545756bde6a682ec6c2e642b3a3d648c3
nhuthq <>
2023-04-18 10:46:47 +0700

    add header

--- booktickets/lib/screens/search_screen.dart
+++ booktickets/lib/screens/search_screen.dart
@@ -2,6 +2,7 @@ import 'package:booktickets/utils/app_layout.dart';
 import 'package:booktickets/utils/app_styles.dart';
 import 'package:booktickets/widgets/double_text_widgets.dart';
 import 'package:booktickets/widgets/icon_text_widget.dart';
+import 'package:booktickets/widgets/top_tabs.dart';
 import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
 
@@ -27,44 +28,7 @@ class SearchScreen extends StatelessWidget {
                 .copyWith(fontSize: AppLayout.getWidth(35)),
           ),
           Gap(AppLayout.getHeight(25)),
-          FittedBox(
-            child: Container(
-              padding: const EdgeInsets.all(3.5),
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(AppLayout.getHeight(50)),
-                  color: const Color(0xFFF4F6FD)),
-              child: Row(
-                children: [
-                  //Airline Tickets
-                  Container(
-                    width: size.width * 0.44,
-                    padding:
-                        EdgeInsets.symmetric(vertical: AppLayout.getHeight(7)),
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.horizontal(
-                            left: Radius.circular(AppLayout.getHeight(50))),
-                        color: Colors.white),
-                    child: const Center(
-                      child: Text("Airline Tickets"),
-                    ),
-                  ),
-                  //Airline Tickets
-                  Container(
-                    width: size.width * 0.44,
-                    padding:
-                        EdgeInsets.symmetric(vertical: AppLayout.getHeight(7)),
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.horizontal(
-                            right: Radius.circular(AppLayout.getHeight(50))),
-                        color: Colors.transparent),
-                    child: const Center(
-                      child: Text("Hotels"),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ),
+          const TopTabs(firstTab: 'Airline Tickets', secondTab: 'Hotels'),
           Gap(AppLayout.getHeight(25)),
           const IconTextWidget(
               icon: Icons.flight_takeoff_rounded, text: "Departure"),
--- booktickets/lib/screens/tickets_screen.dart
+++ booktickets/lib/screens/tickets_screen.dart
@@ -1,14 +1,35 @@
+import 'package:booktickets/utils/app_layout.dart';
+import 'package:booktickets/utils/app_styles.dart';
+import 'package:booktickets/widgets/top_tabs.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:gap/gap.dart';
 
 class TicketsScreen extends StatelessWidget {
   const TicketsScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text("New Tickets Screen"),
+    final size = AppLayout.getSize(context);
+
+    return Scaffold(
+      body: Stack(
+        children: [
+          ListView(
+            padding: EdgeInsets.symmetric(
+                horizontal: AppLayout.getWidth(20),
+                vertical: AppLayout.getHeight(20)),
+            children: [
+              Gap(AppLayout.getHeight(40)),
+              Text(
+                "Tickets",
+                style: Styles.headLineStyle1,
+              ),
+              Gap(AppLayout.getHeight(40)),
+              const TopTabs(firstTab: 'Upcoming', secondTab: 'Previous'),
+            ],
+          )
+        ],
+      ),
     );
   }
 }
