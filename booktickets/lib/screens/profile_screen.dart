c2938330ab22b13d868d73c291b7a8bc7e997ea2
nhuthq <>
2023-04-19 15:26:41 +0700

    setup header

--- booktickets/lib/screens/profile_screen.dart
+++ booktickets/lib/screens/profile_screen.dart
@@ -1,14 +1,104 @@
+import 'package:booktickets/utils/app_layout.dart';
+import 'package:booktickets/utils/app_styles.dart';
+import 'package:fluentui_system_icons/fluentui_system_icons.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:gap/gap.dart';
 
 class ProfileScreen extends StatelessWidget {
   const ProfileScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text("New Profile Screen"),
+    return Scaffold(
+      backgroundColor: Styles.bgColor,
+      body: ListView(
+        padding: EdgeInsets.symmetric(
+          vertical: AppLayout.getHeight(20),
+          horizontal: AppLayout.getWidth(20),
+        ),
+        children: [
+          Gap(AppLayout.getHeight(40)),
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                height: AppLayout.getWidth(86),
+                width: AppLayout.getWidth(86),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(
+                    AppLayout.getHeight(10),
+                  ),
+                  image: const DecorationImage(
+                      image: AssetImage('assets/images/img_1.png')),
+                ),
+              ),
+              Gap(AppLayout.getHeight(10)),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Book Tickets',
+                    style: Styles.headLineStyle1,
+                  ),
+                  Gap(AppLayout.getHeight(2)),
+                  Text(
+                    'Da Nang',
+                    style: Styles.headLineStyle4.copyWith(
+                      color: Colors.grey.shade500,
+                    ),
+                  ),
+                  Gap(AppLayout.getHeight(8)),
+                  Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(
+                        AppLayout.getHeight(20),
+                      ),
+                      color: const Color(0xFFFEF4F3),
+                    ),
+                    child: Row(
+                      children: [
+                        Container(
+                          padding: const EdgeInsets.all(3),
+                          decoration: const BoxDecoration(
+                            shape: BoxShape.circle,
+                            color: Color(0xFF526799),
+                          ),
+                          child: const Icon(FluentIcons.shield_12_filled,
+                              size: 15, color: Colors.white),
+                        ),
+                        Gap(AppLayout.getHeight(5)),
+                        const Text(
+                          "Premium status",
+                          style: TextStyle(
+                              color: Color(0xFF526799),
+                              fontWeight: FontWeight.w600),
+                        ),
+                        Gap(AppLayout.getHeight(5)),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+              const Spacer(),
+              Column(
+                children: [
+                  InkWell(
+                    onTap: () {},
+                    child: Text(
+                      'Edit',
+                      style: Styles.textStyle.copyWith(
+                          color: Styles.primary, fontWeight: FontWeight.w300),
+                    ),
+                  )
+                ],
+              )
+            ],
+          ),
+          Gap(AppLayout.getHeight(8)),
+          Divider(color: Colors.grey.shade300,)
+        ],
+      ),
     );
   }
 }
